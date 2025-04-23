55d403ac6c423bd3af6a162255550932bf25a8ab
Serhii Alieksieienko <in3ruder@hotmail.com>
2025-04-23 11:39:29 +0300

    svg js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -25,32 +25,3 @@ export async function getImagesByQuery(query, page) {
 
 getImagesByQuery();
 
-
-
-
-
-
-
-
-// import axios from "axios";
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
-
-// export { getImagesByQuery }
-
-// const API_KEY = "49643182-9754596b7a63617f9fa0f6656";
-// // axios.defaults.baseURL = 'https://pixabay.com/api/';
-// // axios.defaults.headers.common["key"] = API_KEY;
-// // let page = 2;
-// export default async function getImagesByQuery(query, page) {
-//     return await axios.get('https://pixabay.com/api/', {
-//         params: {
-//         key: API_KEY,
-//         q: query,
-//         image_type: "photo",
-//         orientation: "horizontal",
-//         safesearch: true,
-//         page: page,
-//         per_page: 15
-//     }})
-// }
